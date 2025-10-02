b7e8aefc1bc82c2454300b9a43865ae469cde662
Martin Affolter <martin@affolter.net>
2025-10-02 09:33:08 +0200

    removed project name from secrets configuration

--- affolterNET.Web.Core/Extensions/LocalSecretsExtensions.cs
+++ affolterNET.Web.Core/Extensions/LocalSecretsExtensions.cs
@@ -6,27 +6,35 @@ namespace affolterNET.Web.Core.Extensions;
 
 public static class LocalSecretsExtensions
 {
-    public static IConfigurationBuilder AddLocalSecrets(this IConfigurationBuilder builder, string environment)
+    public static IConfigurationBuilder AddLocalSecrets(this IConfigurationBuilder builder, string environment, string configFile)
     {
         var assemblyLocation = Assembly.GetExecutingAssembly().Location;
         var assemblyDirectory = Path.GetDirectoryName(assemblyLocation) ?? throw new InvalidOperationException("Could not determine assembly directory");
         var secretsDir = Path.Combine(assemblyDirectory, ".secrets");
         
         // Add shared secrets file first (lower priority)
-        var sharedSecretsPath = Path.Combine(secretsDir, "affolterNET.Bexio.json");
+        var sharedSecretsPath = Path.Combine(secretsDir, configFile);
         builder.Add(new LocalSecretsConfigurationSource(sharedSecretsPath));
         
         // Add environment-specific secrets file (higher priority - will override shared values)
+        var fileNameParts = SplitFilename(configFile);
         var environmentSecretsFileName = environment.ToLowerInvariant() switch
         {
-            "development" => "affolterNET.Bexio.development.json",
-            "production" => "affolterNET.Bexio.production.json",
-            _ => $"affolterNET.Bexio.{environment.ToLowerInvariant()}.json"
+            "development" => $"{fileNameParts.Item1}.development{fileNameParts.Item2}",
+            "production" => $"{fileNameParts.Item1}.production{fileNameParts.Item2}",
+            _ => $"{fileNameParts.Item1}.{environment.ToLowerInvariant()}{fileNameParts.Item2}"
         };
         
         var environmentSecretsPath = Path.Combine(secretsDir, environmentSecretsFileName);
         builder.Add(new LocalSecretsConfigurationSource(environmentSecretsPath));
         
         return builder;
+    }
+
+    private static Tuple<string, string> SplitFilename(string fileName)
+    {
+        var withoutExtension = Path.GetFileNameWithoutExtension(fileName);
+        var extension = Path.GetExtension(fileName);
+        return new Tuple<string, string>(withoutExtension, extension);
     }
 }
